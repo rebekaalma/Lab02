22a5b5dfcd55362594c63a530097b1d400385f3c
rebekaalma <rebekabarimbing@gmail.com>
2022-09-13 19:47:53 +0700

    Implement Lab02

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from katalog.views import show_catalog_item
+from django.urls import path
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', show_catalog_item, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_catalog_item(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'nama' : 'Rebeka',
+        'NPM' : '2106653060',
+        'list_catalog': data_catalog_item
+    }
+
+    return render(request, "katalog.html", context)
+
