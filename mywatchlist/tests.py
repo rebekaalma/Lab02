8c62743c56e09d76352e8f7d43fa30ab5b62f5cc
rebekaalma <rebekabarimbing@gmail.com>
2022-09-21 17:50:03 +0700

    Menyelesaikan design html

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+class ContohAppTest(TestCase):
+    def test_contoh_app_url_exist(self):
+        response = Client().get('http://localhost:8000/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+class ContohAppTest(TestCase):
+    def test_contoh_app_url_exist(self):
+        response = Client().get('http://localhost:8000/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+class ContohAppTest(TestCase):
+    def test_contoh_app_url_exist(self):
+        response = Client().get('http://localhost:8000/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
