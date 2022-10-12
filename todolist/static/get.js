6c7d545c3060ed819fa8c6e702500c9f71d09448
rebekaalma <rebekabarimbing@gmail.com>
2022-10-13 03:12:28 +0700

    readmeee

--- todolist/static/get.js
+++ todolist/static/get.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
  
            <h5 class="card-title">${singleData.fields.user}</h5>
            <h6 class="card-subtitle mb-2 text-muted">${singleData.fields.date}</h6>
-           <h6 class="card-subtitle mb-2 text-muted"${singleData.fields.title}</h6>
+           <h6 class="card-subtitle mb-2 text-muted">${singleData.fields.title}</h6>
            <h6 class="card-subtitle mb-2 text-muted">${singleData.fields.is_finished}</h6>
            <p class="card-text">${singleData.fields.description}</p>
            <a href="/todolist/marker/${singleData.pk}   " class="card-link"><button>Update</button></a>
